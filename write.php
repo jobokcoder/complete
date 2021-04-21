fa03c3d00331a370df3f8a90507be06d2dd67555
jobokcoder <zjaqld@naver.com>
2021-04-21 09:54:15 +0900

    Update Write.php

---
+++ write.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/common.css" type="text/css">
+    <link rel="stylesheet" href="./css/write.css" type="text/css">
+    <title>Complete</title>
+</head>
+<body>
+    <div class="wrapper">
+        
+        <?php
+            include('./modules/header.php');
+        ?>
+
+        <div class="contents">
+            <ul class="contents__left">
+                <li class="contents__left--menu">
+                    <a href="#">메뉴</a>
+                    <svg class="menu__hover--svg" xmlns="http://www.w3.org/2000/svg" width="179.745" height="28.979" viewBox="0 0 179.745 28.979">
+                        <path id="패스_89" data-name="패스 89" d="M165.636,500.855v9.582l14.256-9.582,14.723,9.582v-9.582l-14.723-9.582Z" transform="translate(-491.273 194.615) rotate(-90)" fill="#259099"/>
+                        <path id="패스_88" data-name="패스 88" d="M165.636,500.855v9.582l14.256-9.582,14.723,9.582v-9.582l-14.723-9.582Z" transform="translate(671.018 -165.636) rotate(90)" fill="#fbc21c"/>
+                    </svg>
+                </li>
+                <li class="contents__left--menu">
+                    <a href="#">미션</a>
+                    <svg class="menu__hover--svg" xmlns="http://www.w3.org/2000/svg" width="179.745" height="28.979" viewBox="0 0 179.745 28.979">
+                        <path id="패스_89" data-name="패스 89" d="M165.636,500.855v9.582l14.256-9.582,14.723,9.582v-9.582l-14.723-9.582Z" transform="translate(-491.273 194.615) rotate(-90)" fill="#259099"/>
+                        <path id="패스_88" data-name="패스 88" d="M165.636,500.855v9.582l14.256-9.582,14.723,9.582v-9.582l-14.723-9.582Z" transform="translate(671.018 -165.636) rotate(90)" fill="#fbc21c"/>
+                    </svg>
+                </li>
+                <li class="contents__left--menu">
+                    <a href="#">카테고리</a>
+                    <svg class="menu__hover--svg" xmlns="http://www.w3.org/2000/svg" width="179.745" height="28.979" viewBox="0 0 179.745 28.979">
+                        <path id="패스_89" data-name="패스 89" d="M165.636,500.855v9.582l14.256-9.582,14.723,9.582v-9.582l-14.723-9.582Z" transform="translate(-491.273 194.615) rotate(-90)" fill="#259099"/>
+                        <path id="패스_88" data-name="패스 88" d="M165.636,500.855v9.582l14.256-9.582,14.723,9.582v-9.582l-14.723-9.582Z" transform="translate(671.018 -165.636) rotate(90)" fill="#fbc21c"/>
+                    </svg>
+                </li>
+            </ul>
+
+            <form class="write" method="POST">
+                <input class="write__input write__input--title" type="text" name="m_contents" placeholder="제목을 입력하세요."/>
+            </form>
+
+            <div class="contents__right">
+
+            </div>
+        </div>
+    </div>
+<!-- <script src="./js/write.js"></script> -->
+</body>
+</html>
