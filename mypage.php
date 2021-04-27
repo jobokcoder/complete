2b5cafd65f78e6a38205a75937de8e2aed706dd2
jobokcoder <zjaqld@naver.com>
2021-04-27 15:24:56 +0900

    Update Flex to Grid CSS

--- mypage.php
+++ mypage.php
@@ -18,29 +18,10 @@
         ?>
 
         <div class="contents">
-            <ul class="contents__left">
-                <li class="contents__left--menu">
-                    <a href="#">메뉴</a>
-                    <svg class="menu__hover--svg" xmlns="http://www.w3.org/2000/svg" width="179.745" height="28.979" viewBox="0 0 179.745 28.979">
-                        <path id="패스_89" data-name="패스 89" d="M165.636,500.855v9.582l14.256-9.582,14.723,9.582v-9.582l-14.723-9.582Z" transform="translate(-491.273 194.615) rotate(-90)" fill="#259099"/>
-                        <path id="패스_88" data-name="패스 88" d="M165.636,500.855v9.582l14.256-9.582,14.723,9.582v-9.582l-14.723-9.582Z" transform="translate(671.018 -165.636) rotate(90)" fill="#fbc21c"/>
-                    </svg>
-                </li>
-                <li class="contents__left--menu">
-                    <a href="#">미션</a>
-                    <svg class="menu__hover--svg" xmlns="http://www.w3.org/2000/svg" width="179.745" height="28.979" viewBox="0 0 179.745 28.979">
-                        <path id="패스_89" data-name="패스 89" d="M165.636,500.855v9.582l14.256-9.582,14.723,9.582v-9.582l-14.723-9.582Z" transform="translate(-491.273 194.615) rotate(-90)" fill="#259099"/>
-                        <path id="패스_88" data-name="패스 88" d="M165.636,500.855v9.582l14.256-9.582,14.723,9.582v-9.582l-14.723-9.582Z" transform="translate(671.018 -165.636) rotate(90)" fill="#fbc21c"/>
-                    </svg>
-                </li>
-                <li class="contents__left--menu">
-                    <a href="#">카테고리</a>
-                    <svg class="menu__hover--svg" xmlns="http://www.w3.org/2000/svg" width="179.745" height="28.979" viewBox="0 0 179.745 28.979">
-                        <path id="패스_89" data-name="패스 89" d="M165.636,500.855v9.582l14.256-9.582,14.723,9.582v-9.582l-14.723-9.582Z" transform="translate(-491.273 194.615) rotate(-90)" fill="#259099"/>
-                        <path id="패스_88" data-name="패스 88" d="M165.636,500.855v9.582l14.256-9.582,14.723,9.582v-9.582l-14.723-9.582Z" transform="translate(671.018 -165.636) rotate(90)" fill="#fbc21c"/>
-                    </svg>
-                </li>
-            </ul>
+
+            <?php
+                include('./modules/side.php');
+            ?>
 
             <div class="mypage">
                 <div class="mypage__info">
@@ -456,6 +437,42 @@
                             </svg>
                         </div>
                         <div class="request__current--list-wrap picture">
+                            <div class="missions__list">
+                                <div class="missions__list--image">
+                                    <img src="./images/common/common.png" alt="missions" />
+                                    <div class="missions__list--image-opacity"></div>
+                                </div>
+        
+                                <div class="missions__list--info">
+                                    <p class="missions__list--info-title">짐 정리 도와주실분</p>
+                                    <div class="missions__list--info-text">제가 무지 짐 정리를 못하는데 주변에 도와 줄 사람이 없어서 , 주말에 가능.하는데 주변에 도와 줄 사람이 없어서 , 주말에 가능..</div>
+                                    <p class="missions__list--info-writer">의뢰자 : 홍어<br>게시일 : 2021.03.24</p>
+                                </div>
+                            </div>
+                            <div class="missions__list">
+                                <div class="missions__list--image">
+                                    <img src="./images/common/common.png" alt="missions" />
+                                    <div class="missions__list--image-opacity"></div>
+                                </div>
+        
+                                <div class="missions__list--info">
+                                    <p class="missions__list--info-title">짐 정리 도와주실분</p>
+                                    <div class="missions__list--info-text">제가 무지 짐 정리를 못하는데 주변에 도와 줄 사람이 없어서 , 주말에 가능.하는데 주변에 도와 줄 사람이 없어서 , 주말에 가능..</div>
+                                    <p class="missions__list--info-writer">의뢰자 : 홍어<br>게시일 : 2021.03.24</p>
+                                </div>
+                            </div>
+                            <div class="missions__list">
+                                <div class="missions__list--image">
+                                    <img src="./images/common/common.png" alt="missions" />
+                                    <div class="missions__list--image-opacity"></div>
+                                </div>
+        
+                                <div class="missions__list--info">
+                                    <p class="missions__list--info-title">짐 정리 도와주실분</p>
+                                    <div class="missions__list--info-text">제가 무지 짐 정리를 못하는데 주변에 도와 줄 사람이 없어서 , 주말에 가능.하는데 주변에 도와 줄 사람이 없어서 , 주말에 가능..</div>
+                                    <p class="missions__list--info-writer">의뢰자 : 홍어<br>게시일 : 2021.03.24</p>
+                                </div>
+                            </div>
                             <div class="missions__list">
                                 <div class="missions__list--image">
                                     <img src="./images/common/common.png" alt="missions" />
