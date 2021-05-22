bf442b15a2e7af626c77287fcbc0d9f0cb4a61f9
jobokcoder <zjaqld@naver.com>
2021-05-22 22:19:40 +0900

    Update Mission Controller JS

--- js/mission-controller.js
+++ js/mission-controller.js
@@ -1,7 +1,9 @@
 export class missionController{
     constructor(){
         this.app = document.querySelector('.app');
-        this.prvDom;
+        this.prvDom = [];
+        this.stageWidth = document.body.clientWidth;
+        this.stageHeight = document.body.clientHeight;
     }
 
     removeMission(){
@@ -21,6 +23,35 @@ export class missionController{
         dom1_Rect.top + dom1_Rect.height > dom2_Rect.top;
     }
 
+    isCenterCollision(dom1){
+        const dom1_Rect = dom1.getBoundingClientRect();
+
+        let centerX = (this.stageWidth / 2) - 150;
+        let centerY = (this.stageHeight / 2) - 150;
+
+        let centerWidth = 500;
+        let centerHeight = 500;
+
+        return dom1_Rect.left < centerX + centerWidth && 
+        dom1_Rect.left + dom1_Rect.width  > centerX &&
+        dom1_Rect.top < centerY + centerHeight && 
+        dom1_Rect.top + dom1_Rect.height > centerY;
+    }
+
+    getRandomLocation(mission){
+        const locationInfo = {};
+        let maxLocationX = this.stageWidth - Number(mission.style.width.replace('px', ''));
+        let maxLocationY = this.stageHeight - Number(mission.style.height.replace('px', ''));
+
+        let rndLocationX = Math.floor(Math.random() * maxLocationX);
+        let rndLocationY = Math.floor(Math.random() * maxLocationY);
+
+        locationInfo['x'] = rndLocationX;
+        locationInfo['y'] = rndLocationY;
+
+        return locationInfo;
+    }
+
     getMission(name){
         this.info = {
             'name': name,
@@ -38,28 +69,40 @@ export class missionController{
                     newMission.style.width = '200px';
                     newMission.style.height = '300px';
                     newMission.style.background = '#000';
-                    newMission.style.left = 0;
-                    newMission.style.top = 0;
+
+                    let rndLocationInfo = this.getRandomLocation(newMission);
+                    newMission.style.left = `${rndLocationInfo['x']}px`;
+                    newMission.style.top = `${rndLocationInfo['y']}px`;
                     newMission.textContent = item['ms_title'];
 
                     this.app.appendChild(newMission);
 
-                    if(index !== 0 ? this.isCollision(newMission, this.prvDom) : null){
-                        for(let i = 0; i<4; i++){
-                            const newMission_Rect = newMission.getBoundingClientRect();
-                            const prvDom_Rect = this.prvDom.getBoundingClientRect();
+                    if(index !== 0){
+                        this.prvDom.forEach((mission) => {
+                            let flag = this.isCollision(newMission, mission);
+                            while(flag !== false){
+                                let rndLocationInfo = this.getRandomLocation(newMission);
+                                newMission.style.left = `${rndLocationInfo['x']}px`;
+                                newMission.style.top = `${rndLocationInfo['y']}px`;
+                                newMission.textContent = item['ms_title'];
+                                flag = this.isCollision(newMission, mission);
+                            }
+                        });
+
+                        let flag = this.isCenterCollision(newMission);
+                        while(flag !== false){
+                            let rndLocationInfo = this.getRandomLocation(newMission);
+                            newMission.style.left = `${rndLocationInfo['x']}px`;
+                            newMission.style.top = `${rndLocationInfo['y']}px`;
+                            newMission.textContent = item['ms_title'];
+                            flag = this.isCenterCollision(newMission);
                         }
                     }
 
-                    this.prvDom = newMission;
+                    this.prvDom.push(newMission);
                 });
             }
         });
-    }
-
-    resize(stageWidth, stageHeight){
-        this.stageWidth = stageWidth;
-        this.stageHeight = stageHeight;
     }
 }
 
@@ -99,40 +142,39 @@ export class missionController{
 // this.newMission.style.left = `${this.mapRandomCenterX}px`;
 // this.newMission.style.top = `${this.mapRandomCenterY}px`;
 
-        // createLine(moveX, moveY, posX, posY){
-        //     setInterval(() => {
-        //         moveX = moveX + (posX - moveX) * this.speed;
-        //         moveY = moveY + (posY - moveY) * this.speed;
-        //         this.ctx.lineWidth = 2;
-        //         this.ctx.lineCap = 'round';
-        //         this.ctx.beginPath();
-        //         this.ctx.moveTo(this.centerX, this.centerY);
-        //         this.ctx.lineTo(moveX, moveY);
-        //         this.ctx.stroke();
-        //     }, 15);
-        // }
-
-
-        // 겹침 감지 함수 사용 예
-        // this.collisionFlag = this.isCollision(this.prvItem, this.newMission, this.mapRandomCenterX, this.mapRandomCenterY);  
-        
-        // 겹침 감지 함수
-        // isCollision(prv, next, next_x, next_y){
-        //     this.flagArr = [];
-        //     prv.forEach((item) => {
-        //         this.prvItemRect = item.getBoundingClientRect();
-        //         this.flagArr.push(
-        //             this.prvItemRect['x'] < (next_x + next.clientWidth) &&
-        //             (this.prvItemRect['x'] + this.prvItemRect['width']) > next_x &&
-        //             this.prvItemRect['y'] < (next_y + next.clientHeight) &&
-        //             (this.prvItemRect['y'] + this.prvItemRect['height']) > next_y ||
-
-        //             ((document.body.clientWidth / 2) - 200) < (next_x + next.clientWidth) &&
-        //             ((document.body.clientWidth / 2) - 200 + 400) > next_x
-        //         );
-        //     });
-            
-        //     return this.flagArr.every((flag) => {
-        //         return flag === true ? false : true;
-        //     });
-        // }
+// createLine(moveX, moveY, posX, posY){
+//     setInterval(() => {
+//         moveX = moveX + (posX - moveX) * this.speed;
+//         moveY = moveY + (posY - moveY) * this.speed;
+//         this.ctx.lineWidth = 2;
+//         this.ctx.lineCap = 'round';
+//         this.ctx.beginPath();
+//         this.ctx.moveTo(this.centerX, this.centerY);
+//         this.ctx.lineTo(moveX, moveY);
+//         this.ctx.stroke();
+//     }, 15);
+// }
+//
+// 겹침 감지 함수 사용 예
+// this.collisionFlag = this.isCollision(this.prvItem, this.newMission, this.mapRandomCenterX, this.mapRandomCenterY);  
+//
+// 겹침 감지 함수
+// isCollision(prv, next, next_x, next_y){
+//     this.flagArr = [];
+//     prv.forEach((item) => {
+//         this.prvItemRect = item.getBoundingClientRect();
+//         this.flagArr.push(
+//             this.prvItemRect['x'] < (next_x + next.clientWidth) &&
+//             (this.prvItemRect['x'] + this.prvItemRect['width']) > next_x &&
+//             this.prvItemRect['y'] < (next_y + next.clientHeight) &&
+//             (this.prvItemRect['y'] + this.prvItemRect['height']) > next_y ||
+//
+//             ((document.body.clientWidth / 2) - 200) < (next_x + next.clientWidth) &&
+//             ((document.body.clientWidth / 2) - 200 + 400) > next_x
+//         );
+//     });
+//
+//     return this.flagArr.every((flag) => {
+//         return flag === true ? false : true;
+//     });
+// }
