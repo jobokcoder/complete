b0722404f1712972a9176557e36f364f4de801a0
jobokcoder <zjaqld@naver.com>
2021-05-31 11:52:24 +0900

    Update Event Page - element Add

--- event.php
+++ event.php
@@ -28,11 +28,12 @@
                 <div class="event__contents">
                     <div class="event__contents--item">
                         <div class="event__contents--item-img">
-                            <img src="./images/common/flower.jpg" alt="event"/>
+                            <img src="./images/common/0515.jpg" alt="event"/>
                         </div>
                         
                         <div class="event__contents--hashBox">
                             <b class="event__contents--hashTag"># 스승의 날</b>
+                            <b class="event__contents--hashTag"># 스승의 은혜</b>
                             <b class="event__contents--hashTag"># 꽃 선물</b>
                         </div>
 
@@ -44,82 +45,36 @@
                     
                     <div class="event__contents--item">
                         <div class="event__contents--item-img">
-                            <img src="./images/common/flower.jpg" alt="event"/>
+                            <img src="./images/common/0508.jpg" alt="event"/>
                         </div>
                         
                         <div class="event__contents--hashBox">
-                            <b class="event__contents--hashTag"># 스승의 날</b>
+                            <b class="event__contents--hashTag"># 어버이날</b>
                             <b class="event__contents--hashTag"># 꽃 선물</b>
+                            <b class="event__contents--hashTag"># 부모님</b>
                         </div>
 
-                        <p class="event__contents--title">교수님께 꽃 선물하기</p>
-                        <p class="event__contents--subtitle">스승의 날</p>
+                        <p class="event__contents--title">부모님께 어버이날 선물하기</p>
+                        <p class="event__contents--subtitle">어버이날</p>
 
-                        <p class="event__contents--date">2021.5.12 ~ 2021.5.19</p>
+                        <p class="event__contents--date">2021.5.6 ~ 2021.5.10</p>
                     </div>
 
                     <div class="event__contents--item">
                         <div class="event__contents--item-img">
-                            <img src="./images/common/flower.jpg" alt="event"/>
+                            <img src="./images/common/0505.png" alt="event"/>
                         </div>
                         
                         <div class="event__contents--hashBox">
-                            <b class="event__contents--hashTag"># 스승의 날</b>
-                            <b class="event__contents--hashTag"># 꽃 선물</b>
+                            <b class="event__contents--hashTag"># 오늘은</b>
+                            <b class="event__contents--hashTag"># 어린이날</b>
+                            <b class="event__contents--hashTag"># 어른이날(?)</b>
                         </div>
 
-                        <p class="event__contents--title">교수님께 꽃 선물하기</p>
-                        <p class="event__contents--subtitle">스승의 날</p>
+                        <p class="event__contents--title">자녀 동생에게 즐거운 하루 보내주기</p>
+                        <p class="event__contents--subtitle">어린이날</p>
 
-                        <p class="event__contents--date">2021.5.12 ~ 2021.5.19</p>
-                    </div>
-
-                    <div class="event__contents--item">
-                        <div class="event__contents--item-img">
-                            <img src="./images/common/flower.jpg" alt="event"/>
-                        </div>
-                        
-                        <div class="event__contents--hashBox">
-                            <b class="event__contents--hashTag"># 스승의 날</b>
-                            <b class="event__contents--hashTag"># 꽃 선물</b>
-                        </div>
-
-                        <p class="event__contents--title">교수님께 꽃 선물하기</p>
-                        <p class="event__contents--subtitle">스승의 날</p>
-
-                        <p class="event__contents--date">2021.5.12 ~ 2021.5.19</p>
-                    </div>
-
-                    <div class="event__contents--item">
-                        <div class="event__contents--item-img">
-                            <img src="./images/common/flower.jpg" alt="event"/>
-                        </div>
-                        
-                        <div class="event__contents--hashBox">
-                            <b class="event__contents--hashTag"># 스승의 날</b>
-                            <b class="event__contents--hashTag"># 꽃 선물</b>
-                        </div>
-
-                        <p class="event__contents--title">교수님께 꽃 선물하기</p>
-                        <p class="event__contents--subtitle">스승의 날</p>
-
-                        <p class="event__contents--date">2021.5.12 ~ 2021.5.19</p>
-                    </div>
-
-                    <div class="event__contents--item">
-                        <div class="event__contents--item-img">
-                            <img src="./images/common/flower.jpg" alt="event"/>
-                        </div>
-                        
-                        <div class="event__contents--hashBox">
-                            <b class="event__contents--hashTag"># 스승의 날</b>
-                            <b class="event__contents--hashTag"># 꽃 선물</b>
-                        </div>
-
-                        <p class="event__contents--title">교수님께 꽃 선물하기</p>
-                        <p class="event__contents--subtitle">스승의 날</p>
-
-                        <p class="event__contents--date">2021.5.12 ~ 2021.5.19</p>
+                        <p class="event__contents--date">2021.5.02 ~ 2021.5.06</p>
                     </div>
                 </div>
             </div>
