936388687f16289d3146ea065dcd4444eb1843a4
jobokcoder <zjaqld@naver.com>
2021-06-02 09:10:33 +0900

    Update Event Banner CSS

--- modules/getSlideMission.php
+++ modules/getSlideMission.php
@@ -15,7 +15,7 @@
         if($type === 1){
             $sql = "select mission.*, member.* from mission join member on mission.ms_writer = member.m_id where member.m_add1 = '$area' order by mission.ms_lookup desc limit 0, 8";
         }else if($type === 2){
-            $sql = "select mission.*, member.* from mission join member on mission.ms_writer = member.m_id where member.m_add1 = '$area' order by mission.ms_date_end desc limit 0, 8";
+            $sql = "select mission.*, member.* from mission join member on mission.ms_writer = member.m_id where member.m_add1 = '$area' order by mission.ms_date_end asc limit 0, 8";
         }
     }
 
