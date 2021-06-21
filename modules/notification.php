2e10bb8113fe16a457a7030e73c705abdfb5ee54
jobokcoder <zjaqld@naver.com>
2021-06-21 20:47:02 +0900

    Add

---
+++ modules/notification.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div style="border: 1px solid #000000; max-width: 700px; overflow: hidden; box-sizing: border-box; margin: 0; padding: 5vw; font-family:'맑은고딕','Malgun Gothic','나눔고딕','NanumGothic',dotum,'돋움',sans-serif; ">
+        <div style=" border-collapse: collapse; box-sizing: border-box; margin: 0; width: 100% ; max-width: 500px; background-color: #fff; display: table; table-layout: fixed; ">
+            <div style="border-collapse: collapse; box-sizing: border-box; margin: 0; padding: 0; width: 100%; height: 50px; display: table-row; border-bottom: 4px solid #0D295D;">
+                <div style="display: table-cell; width: 100%;">
+                    <h1 style="float: left; font-size: 24px; font-family:'맑은고딕','Malgun Gothic','나눔고딕','NanumGothic',dotum,'돋움',sans-serif;">Complete 알리미</h1>
+                    <div style="float: right; width: 30px;  transform: translateY(20%);">
+                        <img style="width: 30px;" src="https://user-images.githubusercontent.com/73686581/115854010-c395ee80-a464-11eb-9fc4-4944ab60e19d.png" alt="logo" />
+                    </div>
+                </div>
+            </div>
+            <div style="display: table-row; width: 100%;">
+                <div style="display: table-cell; box-sizing: border-box; padding-top: 50px; width: 100%; font-size: 18px; font-family:'맑은고딕','Malgun Gothic','나눔고딕','NanumGothic',dotum,'돋움',sans-serif;">
+                    <p style="margin: 0; padding: 0; letter-spacing: -1.2px;">
+                        complete 가입을 위한 단계입니다. 계정을 생성하기 전에<br>
+                        이 이메일 주소가 계정에 쓰일올바른 주소가 맞는지 확인해주세요. <br><br>
+                        complete를 시작하려면 다음 인증 코드를 입력하세요.<br><br>
+                        <strong>{code}</strong><br><br>
+                        감사합니다.<br>
+                        complete
+                    </p>
+                </div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
